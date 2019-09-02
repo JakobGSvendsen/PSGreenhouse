e11ccef60946f646e1dbb383362cf21f1f60685e
Jakob Gottlieb Svendsen <3460807+JakobGSvendsen@users.noreply.github.com>
2019-09-02 19:12:59 +0200

    Update ghouse-init.sh

--- ghouse-init.sh
+++ ghouse-init.sh
@@ -1,4 +1,4 @@
 ﻿#!/bin/sh -e
 motion
 docker build --tag greenhouse https://raw.githubusercontent.com/JakobGSvendsen/PSGreenhouse/master/Dockerfile;
-docker run --interactive --tty --privileged -v /home/pi/pswh/GHouseConfig.json:/root/greenhouse/GHouseConfig.json greenhouse;
+docker run --interactive --tty --privileged -v /home/pi/pswh/GHouseConfig.json:/root/greenhouse/GHouseConfig.json  --restart always greenhouse;
