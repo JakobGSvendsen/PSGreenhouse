f2e0744b6b47c6c19b8f07d22a0727ce65055fea
JakobGSvendsen <jakob.svendsen@gmail.com>
2019-09-25 22:24:23 +0200

    ghouse-init

--- ghouse-init.sh
+++ ghouse-init.sh
@@ -1,4 +1,2 @@
-﻿#!/bin/sh -e
-motion
-docker build --tag greenhouse https://raw.githubusercontent.com/JakobGSvendsen/PSGreenhouse/master/Dockerfile;
-docker run --interactive --rm --tty --privileged -v /home/pi/pswh/GHouseConfig.json:/root/greenhouse/GHouseConfig.json greenhouse;
+﻿docker build --tag greenhouse https://raw.githubusercontent.com/JakobGSvendsen/PSGreenhouse/master/Dockerfile
+docker run --interactive --rm --tty --privileged -v /home/pi/pswh/GHouseConfig.json:/root/greenhouse/GHouseConfig.json greenhouse
